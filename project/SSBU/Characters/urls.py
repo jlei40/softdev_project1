c2d9e3620f50aaf48731dc325d8d12e441997ea1
jlei40 <124059344+jlei40@users.noreply.github.com>
2023-10-13 21:27:31 -0400

    upladonig images to all the characters

--- project/SSBU/Characters/urls.py
+++ project/SSBU/Characters/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = "characters"
 
 urlpatterns = [
-   path("index", views.index, name="index"),
+   path("", views.index, name="index"),
    path("cookies", views.cookies, name="cookies"),
    path("djangoforms", views.djangoforms, name="djangoforms"),
    path("nondjangoforms", views.nondjangoforms, name="nondjangoforms"),   
-   path("", views.character_info, name="character_info"),
+   path("character_info", views.character_info, name="character_info"),
    path("search", views.search, name="search"),
    path("searched",views.searched, name="searched"),
 
