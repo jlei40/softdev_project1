c360509bd84279c960a3de56bfc3e3586dffa7ac
John Chandler <jchandleriii40@stuy.edu>
2023-10-13 21:36:19 -0400

    all core functions good

--- project/SSBU/Characters/urls.py
+++ project/SSBU/Characters/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
    path("cookies", views.cookies, name="cookies"),
    path("djangoforms", views.djangoforms, name="djangoforms"),
    path("nondjangoforms", views.nondjangoforms, name="nondjangoforms"),   
-   path("", views.character_info, name="character_info"),
+   path("character_info", views.character_info, name="character_info"),
    path("search", views.search, name="search"),
    path("searched",views.searched, name="searched"),
 
